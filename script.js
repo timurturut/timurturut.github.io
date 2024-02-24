736c32a676156529c190d5f6044a95742fb655fb
timurturut <timur21turut@gmail.com>
2024-02-24 13:03:59 +0300

    hamburger

--- script.js
+++ script.js
@@ -16,3 +16,19 @@ function toggleMenu() {
         elements.style.display = "none";
     }
 }
+
+const hamburger = document.querySelector(".hamburger");
+const middleElements = document.querySelector(".middleElements")
+
+hamburger.addEventListener("click", () =>{
+    hamburger.classList.toggle("active")
+    middleElements.classList.toggle("active")
+})
+
+
+document.querySelectorAll(".middleElements").forEach(n => n.
+    addEventListener("click" ,  () => {
+        hamburger.classList.remove("active")
+        middleElements.classList.remove("active")
+    })
+)
